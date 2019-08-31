6d0e501ce6febd6612a5247a24e9ad0f8a1274bc
JT <coreprogramm@gmail.com>
2019-09-01 00:30:16 +0530

    Disqus Integration in .NET Core
    In this CoreProgramm we found that how to implement Disqus Integration in .NET Core

---
+++ DisqusIntegrationCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DisqusIntegrationCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
